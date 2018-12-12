3e60b72cf828109ff5d7b12aad26b5a981fc77e3
Kamlesh Tajpuri <kamlesh.tajpuri.contractor@move.com>
2018-12-12 19:26:26 +0530

    solution using class

--- detectFace.js
+++ detectFace.js
@@ -1,65 +1,77 @@
-const AWS = require('aws-sdk');
-const sharp = require('sharp');
+const AWS = require("aws-sdk");
+const sharp = require("sharp");
 
 AWS.config.update({
-    region: 'us-east-2'
+  region: "us-east-2"
 });
 const rekognition = new AWS.Rekognition();
-
-const detectFace = (imageBuffer) => {
-    const params = {
-        Image: {
-            Bytes: imageBuffer
-        },
-        Attributes: [
-            'ALL',
-        ]
+class AWSRekognition extends sharp {
+  constructor(input) {
+    super(input);
+    this.toBuffer = () => {
+      return super.toBuffer().then(data => {
+        this.buffer = data;
+        return new Promise(function(resolve, reject) {
+          resolve();
+        });
+      });
     };
-    rekognition.detectFaces(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(JSON.stringify(data, null, 4));           // successful response
-    });
-};
 
-
-const recognizeCelebrities = (imageBuffer) => {
-    const params = {
+    this.detectFace = () => {
+      const params = {
         Image: {
-            Bytes: imageBuffer
+          Bytes: this.buffer
+        },
+        Attributes: ["ALL"]
+      };
+      rekognition.detectFaces(params, this.print);
+    };
+
+    this.recognizeCelebrities = () => {
+      const params = {
+        Image: {
+          Bytes: this.buffer
         }
+      };
+      rekognition.recognizeCelebrities(params, this.print);
     };
-    rekognition.recognizeCelebrities(params, (err, data) => {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(JSON.stringify(data, null, 4));           // successful response
-    });
-};
 
-const detectObjects = (imageBuffer) => {
-    const params = {
+    this.detectObjects = () => {
+      const params = {
         Image: {
-            Bytes: imageBuffer
+          Bytes: this.buffer
         },
-        MaxLabels: 123, 
+        MaxLabels: 123,
         MinConfidence: 70
+      };
+      rekognition.detectLabels(params, this.print);
     };
-    rekognition.detectLabels(params, (err, data) => {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(JSON.stringify(data, null, 4));           // successful response
-    });
-};
 
-const detecttext = (imageBuffer) => {
-    const params = {
+    this.detecttext = () => {
+      const params = {
         Image: {
-            Bytes: imageBuffer
+          Bytes: this.buffer
         }
+      };
+      rekognition.detectText(params, this.print);
     };
-    rekognition.detectText(params, (err, data) => {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(JSON.stringify(data, null, 4));           // successful response
-    });
-};
 
-sharp('imageWithFace.jpg')
-    .toBuffer()
-    .then(recognizeCelebrities);
+    this.print = (err, data) => {
+        if (err) console.log(err, err.stack);
+        // an error occurred
+        if(data) console.log(JSON.stringify(data, null, 4)); // successful response
+        return this;
+    }
+
+    this.run = () => {
+        this.toBuffer()
+        //.then(this.recognizeCelebrities)
+        .then(this.detectFace)
+        .then(this.detecttext)
+        //.then(this.detectObjects)
+        .then(this.print)
+    };
+  }
+}
+
+new AWSRekognition("text.png").run();
